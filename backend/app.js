1c172a2a294cdeac916dc86b597f9997199b20b5
jisus-saurio <stanleyarce51@gmail.com>
2025-06-19 13:35:57 -0600

    Produccion2

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,27 @@
 import express from "express";
 import tasksRoutes from "./src/routes/tasks.js";
 import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
 
-// Inicializar express
+// Creo una constante que es igual a la libreria que importé
 const app = express();
 
-// Middleware para aceptar datos en JSON
+// Que acepte datos en json
 app.use(express.json());
-// Middleware para cookies
+// Para que postman guarde el token en una cookie
 app.use(cookieParser());
 
+// Documentación Swagger
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve("./Bryan.json"), "utf8")
+);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Definir las rutas de las funciones que tendrá la página web
 app.use("/api/tasks", tasksRoutes);
 
-// Exportar app
+// Exporto la constante para poder usar express en otros archivos
 export default app;
